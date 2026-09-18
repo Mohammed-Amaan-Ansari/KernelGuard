e35cfbf7e7fdf9c35460657f2a2002ca12deef53
Mohammed Amaan Ansari <ansari.mdamaan1@gmail.com>
2026-09-18 04:05:57 +0000

    Created a Test file

---
+++ examples/write_test.py
@@ -0,0 +1,12 @@
+import os
+import time
+
+path = "/tmp/kernelguard_test.txt"
+
+print(f"Test process PID: {os.getpid()}")
+
+while True:
+    with open(path, "a", encoding="utf-8") as file:
+        file.write("KernelGuard write test\n")
+
+    time.sleep(2)
